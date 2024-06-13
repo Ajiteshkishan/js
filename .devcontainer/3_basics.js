364450a3e0b50c0f4b11596ae291981975bae5f4
Ajiteshkishan <ajiteshkishan118@gmail.com>
2024-06-13 08:13:10 +0000

    conversion table

---
+++ .devcontainer/3_basics.js
@@ -0,0 +1,32 @@
+// let score ="1a" ;// this is string
+// let skore= 33; // this is number 
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// // type conversion.
+// let valueinnumber = Number(score);
+// console.log((valueinnumber));
+// // "33" => 33
+// //"33abc " => nan
+// // true  => 1;false => 0
+
+// // output id = nan -> is a special type 
+// // if there is no value in then it will give output as null
+// console.log(typeof(valueinnumber));
+
+
+//conversion of boolean into numbers
+// let isLoggedIn = 1;
+// // 1 => true
+// //0 => false 
+// // "" => false , empty string false 
+// //"udh" => true
+// let booleanisloggedin = Boolean(isLoggedIn);
+// console.log(booleanisloggedin);
+
+
+// conversion into string 
+let somenumber = 33;
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
